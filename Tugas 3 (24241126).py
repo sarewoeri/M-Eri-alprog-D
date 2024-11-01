de42d24c4173e044ddb6f32180a17d6e0afa72e8
sarewoeri <muhammaderierieri2004@gmail.com>
2024-11-01 19:34:26 +0800

    Add files via upload

---
+++ Tugas 3 (24241126).py
@@ -0,0 +1,6 @@
+# Buatlah program dengan output sebagai berikut
+print("nama saya adalah muhammad eri")
+print("nim saya adalah (24241126)")
+print("saya berasal dari bima")
+print("nomor keberuntungan saya(24+26)")
+print("50")
