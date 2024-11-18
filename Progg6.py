56fa7e04e70039209b8db781fc63794a21198624
sarewoeri <muhammaderierieri2004@gmail.com>
2024-11-18 08:38:20 +0800

    Add files via upload

---
+++ Progg6.py
@@ -0,0 +1,12 @@
+#luas persegi prog 2
+p= 26
+l = 24
+L= 276.0
+
+#casting tipe data
+p = int(input("masukkan nilai p:"))
+print (" nilai p adalah : ", p, type(p))
+l = int(input("masukkan nilai l:"))
+print (" nilai l adalah : ", l, type(l))
+L= float(input("masukkan nilai L:"))
+print (" nilai L adalah : ", L, type(L))
