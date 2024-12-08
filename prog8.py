3b815766630669a864a1025546cf5be98a476884
sarewoeri <muhammaderierieri2004@gmail.com>
2024-12-08 10:36:56 +0800

    Add files via upload

---
+++ prog8.py
@@ -0,0 +1,32 @@
+#operasi aritmatika
+
+a = 24
+b = 26
+
+#operasi kali x
+hasil = a * b
+print("nilai dari a * b =",hasil)
+
+#operasi bagi :
+hasil = a / bagi
+print("nilai dari a / b =",hasil)
+
+#operasi tambah +
+hasil = a + b 
+print("nilai dari a + b =,"hasil)
+
+#operasi kurang -
+hasil = a - b
+print("nilai dari a - b =",hasil)
+
+#operasi perpangkatan **
+hasil = a ** b
+print("nilai dari a ** b =",hasil)
+
+#modulus (sisa bagi) %
+hasil = a % b
+print("nilai dari a % b =",hasil) 
+
+#floor dividion //
+hasil = a // b
+print("nilai dari a // b =",hasil)
