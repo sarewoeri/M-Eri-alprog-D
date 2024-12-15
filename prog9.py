aad27c355d685cb7d85b3e99e48191e4776ebe9d
sarewoeri <muhammaderierieri2004@gmail.com>
2024-12-15 10:44:12 +0800

    Add files via upload

---
+++ prog9.py
@@ -0,0 +1,17 @@
+#suhu celcius = 26
+# suhu dalam Fahrenheit = 75.2
+# suhu dalam kelvin = 297,15
+# suhu dalam reamur = 19,2
+
+# suhu dalam celcius
+celcius = 26
+
+# rumus konversi suhu
+derajat fahrenheit =(celciusm * 9/5)+ 32
+kelvin = celcius + 273.15
+tempat tidur = celcius * 4/5
+
+# cetak nilai suhu
+print("suhu dalam fahrenheit:", fahrenheit)
+print("suhu dalam kelvin:", kelvin)
+print("suhu dalam reamur:", reamur)
