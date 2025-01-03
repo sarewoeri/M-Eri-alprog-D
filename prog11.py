6ac0262ac67f9c40d762862cabe8a24d845aa99e
sarewoeri <muhammaderierieri2004@gmail.com>
2025-01-03 15:51:25 +0800

    Add files via upload

---
+++ prog11.py
@@ -0,0 +1,30 @@
+# Logika bolean
+
+# not and or xor
+
+print ("==========not==========")
+x = false
+z = not x
+print("nilai z = ", z)
+
+#(jika ada salah, maka hasilnya salah)
+print("========AND========")
+x = False
+y = False
+nilai = x and y
+print (x, "and", y, "=", nilai)
+
+x = False
+y = True
+nilai = x and y
+print(x, "and", y, "=", nilai)
+
+x = True
+y = False
+nilai = x and y
+print(x, "and", y, "=", nilai)
+
+x = True
+y = True
+nilai = x and y
+print(x, "and", y, "=", nilai)
